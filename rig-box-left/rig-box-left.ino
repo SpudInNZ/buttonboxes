8bd671072ca897cfef8ce2d5eb0c69885680c9a6
Ian <ian@bevantech.com>
2021-07-03 17:25:35 +1200

    Left box source

---
+++ rig-box-left/rig-box-left.ino
@@ -0,0 +1,37 @@
+// Left button box, 4 buttons. Based on joystick example code.
+#include <Joystick.h>
+
+Joystick_ Joystick;
+
+void setup() {
+  // Initialize Button Pins
+  pinMode(6, INPUT_PULLUP);
+  pinMode(7, INPUT_PULLUP);
+  pinMode(8, INPUT_PULLUP);
+  pinMode(9, INPUT_PULLUP);
+
+  // Initialize Joystick Library
+  Joystick.begin();
+}
+
+// Constant that maps the phyical pin to the joystick button.
+const int pinToButtonMap = 6;
+
+// Last state of the button
+int lastButtonState[4] = {0,0,0,0};
+
+void loop() {
+
+  // Read pin values
+  for (int index = 0; index < 4; index++)
+  {
+    int currentButtonState = !digitalRead(index + pinToButtonMap);
+    if (currentButtonState != lastButtonState[index])
+    {
+      Joystick.setButton(index, currentButtonState);
+      lastButtonState[index] = currentButtonState;
+    }
+  }
+
+  delay(50);
+}
